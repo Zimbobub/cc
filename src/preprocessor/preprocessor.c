8e87dff6745dd309ea77c90c4f4ebd9657c70182
Ayla Oliver <zimbobub@protonmail.com>
2026-08-11 18:42:56 +1000

    delete old run_preprocessor()

--- src/preprocessor/preprocessor.c
+++ src/preprocessor/preprocessor.c
@@ -1,171 +1,5 @@
 #include "preprocessor.h"
 
-TokenType get_single_char_token_type(char c);
-TokenType get_double_char_token_type(char c1, char c2);
-
-// void old_run_preprocessor(const char *file_name, Token *tokens, size_t *num_tokens) {
-//     FILE *fp = fopen(file_name, "r");
-
-//     if (fp == NULL) {
-//         printf("Unable to open file.");
-//         exit(EXIT_FAILURE);
-//     }
-
-//     size_t current_token_max_size = 100;
-//     size_t current_token_size = 0;
-//     char* current_token = malloc(sizeof(char) * current_token_max_size);
-
-//     size_t token_buf_size = 1000;
-//     tokens = malloc(sizeof(Token) * token_buf_size);
-
-//     if (current_token == NULL || tokens == NULL) {
-//         printf("Failed to allocate preprocessor.");
-//         fclose(fp);
-//         exit(EXIT_FAILURE);
-//     }
-
-//     memset(current_token, 0, current_token_max_size);
-//     *num_tokens = 0;
-
-//     // tokenise loop
-//     char c;
-//     size_t line = 1;
-//     size_t col = 0;
-
-//     #define IS_IDENTIFIER 0x1
-//     #define IS_NUMBER 0x2
-//     #define IS_SINGLE_CHAR 0x4
-//     #define IS_DOUBLE_CHAR 0x8
-//     uint8_t prev_token_type_bitset = 0b00001111;
-//     uint8_t token_type_bitset = 0b00001111;
-
-//     while ((c = fgetc(fp)) != EOF) {
-//         if (c == '\n') {
-//             line++;
-//             col = 0;
-//         }
-//         col++;
-
-//         if (current_token_size == 0 && isspace(c)) continue;
-
-
-//         // keyword / identifier
-//         if (current_token_size == 0 && isdigit(c)) token_type_bitset &= ~IS_IDENTIFIER;
-//         if (!isalnum(c) && c != '_') token_type_bitset &= ~IS_IDENTIFIER;
-
-//         // number literal
-//         if (!isdigit(c)) token_type_bitset &= ~IS_NUMBER;
-
-//         // single / double char
-//         if (current_token_size > 1 || 
-//             (current_token_size == 1 && get_single_char_token_type(current_token[0]) == Unknown) ||
-//             !ispunct(current_token[0])
-//         ) {
-//             token_type_bitset &= ~IS_SINGLE_CHAR;
-//         }
-//         if (current_token_size > 2 || 
-//             (current_token_size == 2 && get_double_char_token_type(current_token[0], current_token[1]) == Unknown) ||
-//             (current_token_size == 1 && get_single_char_token_type(current_token[0]) != Unknown) ||
-//             !ispunct(current_token[0]) || !ispunct(current_token[1])
-//         ) {
-//             token_type_bitset &= ~IS_DOUBLE_CHAR;
-//         }
-
-//         printf("%ld:%ld %c (0x%d, 0x%d) '%s' %ld\n", line, col, c, prev_token_type_bitset, token_type_bitset, current_token, current_token_size);
-
-//         // once token_type_bitset gets to 0, see which type in the previous loop had its bit set last
-//         if (token_type_bitset == 0 || 
-//             token_type_bitset == IS_SINGLE_CHAR ||
-//             token_type_bitset == IS_DOUBLE_CHAR
-//         ) {
-//             Token token = {0};
-//             token.line_num = line;
-//             token.col_num = col;
-
-//             size_t chars_consumed = 0;
-
-//             if (prev_token_type_bitset & IS_IDENTIFIER) {
-//                 token.type = Identifier;
-//                 chars_consumed = current_token_size;
-//             }
-//             else if (prev_token_type_bitset & IS_NUMBER) {
-//                 token.type = Number;
-//                 chars_consumed = current_token_size;
-//             }
-//             else if (prev_token_type_bitset & IS_SINGLE_CHAR) {
-//                 token.type = get_single_char_token_type(current_token[0]);
-//                 chars_consumed = 1;
-//             }
-//             else if (prev_token_type_bitset & IS_DOUBLE_CHAR) {
-//                 token.type = get_double_char_token_type(current_token[0], current_token[1]);
-//                 chars_consumed = 2;
-//             }
-//             else {
-//                 printf("Unknown token type %x at %ld:%ld, '%s'\n", prev_token_type_bitset, line, col, current_token);
-//                 fclose(fp);
-//                 exit(EXIT_FAILURE);
-//             }
-
-//             // copy src
-//             token.src = malloc(chars_consumed+1);
-//             if (token.src == NULL) {
-//                 printf("Failed to allocate token.");
-//                 fclose(fp);
-//                 exit(EXIT_FAILURE);
-//             }
-//             strncpy(token.src, current_token, chars_consumed);
-
-//             print_token(&token);
-
-//             tokens[*num_tokens] = token;
-//             (*num_tokens)++;
-
-
-//             // reset current_token
-//             // trim whitespace at the start
-//             size_t spaces = 0;
-//             while (isspace(current_token[chars_consumed + spaces])) spaces++;
-//             // shift all characters to the left for each char consumed
-//             strcpy(current_token, current_token + chars_consumed + spaces);
-//             // set all characters after the \0 to \0
-//             memset(current_token + (current_token_size - chars_consumed), 0, current_token_max_size - chars_consumed);
-
-//             current_token_size = strlen(current_token);
-
-
-//             // reset bitsets
-//             token_type_bitset = 0b00001111;
-//             prev_token_type_bitset = 0b00001111;
-//         } else {
-//             prev_token_type_bitset = token_type_bitset;
-//         }
-//         // add char to buffer
-//         if (!isspace(c)) {
-//             // grow current_token each loop until only 1 token type bool remains
-//             if (current_token_size >= current_token_max_size-1) {
-//                 current_token_max_size *= 2;
-//                 current_token = realloc(current_token, current_token_max_size);
-//                 if (current_token == NULL) {
-//                     printf("Failed to reallocate.");
-//                     fclose(fp);
-//                     exit(EXIT_FAILURE);
-//                 }
-//             }
-
-//             current_token[current_token_size] = c;
-//             current_token_size++;
-//         }
-//     }
-
-//     fclose(fp);
-
-//     if (current_token_size > 0) {
-//         printf("Error: Finished parsing with '%s' remaining at EOF (make sure to add a newline at the end of the file)\n", current_token);
-//         exit(EXIT_FAILURE);
-//     }
-// }
-
-
 TokenType get_single_char_token_type(char c) {
     if (c == ';') return Semicolon;
     else if (c == '.') return Period;
@@ -194,7 +28,7 @@ bool is_delimiter(char c) {
 bool is_identifier(const char* str, size_t len) {
     if (len == 0) return false;
     for (int i = 0; i < len; i++) {
-        if (is_delimiter(str[i])) return false;
+        if (!isalnum(str[i]) && str[i] != '_') return false;
     }
     return (!isdigit(str[0]));
 }
@@ -225,7 +59,16 @@ bool is_int(const char *str, size_t len) {
     return true;
 }
 
-void add_token(Preprocessor *this,  TokenType type) {
+//
+// PREPROCESSOR HELPER METHODS
+//
+
+void clear_buffer(Preprocessor *this) {
+    memset(this->input_buf, 0, this->input_buf_max_size);
+    this->input_buf_size = 0;
+}
+
+void consume_token(Preprocessor *this,  TokenType type) {
     // make token
     Token token = {0};
     token.line_num = this->line;
@@ -277,11 +120,6 @@ void consume_chars(Preprocessor *this, size_t num_chars) {
     strcpy(this->input_buf, temp_str);
 }
 
-void clear_buffer(Preprocessor *this) {
-    memset(this->input_buf, 0, this->input_buf_max_size);
-    this->input_buf_size = 0;
-}
-
 Preprocessor* preprocessor_init(const char* file_name) {
     Preprocessor *this = malloc(sizeof(Preprocessor));
     if (this == NULL) {
@@ -323,45 +161,68 @@ void preprocessor_run(Preprocessor *this) {
 
     int c;
     while (c != EOF) {
+        c = fgetc(this->fp);
+        if (c == EOF) break;
+
         // if within a token, add next char to buf
         if (!is_delimiter(this->input_buf[this->input_buf_size-1])) {
-            c = fgetc(this->fp);
             add_char(this, c);
         } 
 
         // single char token
         if (is_delimiter(this->input_buf[this->input_buf_size-1]) && this->input_buf_size == 1) {
+            // try make 2 char special
+            c = fgetc(this->fp);
+            TokenType double_token = get_double_char_token_type(this->input_buf[0], c);
+            if (double_token != Unknown) {
+                // 2 char
+                printf("token '%c'\n", this->input_buf[0]);
+                add_char(this, c);
+                consume_token(this, double_token);
+            } else {
+                // 1 char
+                TokenType single_token = get_single_char_token_type(this->input_buf[0]);
+                if (single_token == Unknown) {
+                    printf("Unknown single char token type: '%c'\n", this->input_buf[0]);
+                    fclose(this->fp);
+                    exit(EXIT_FAILURE);
+                }
+                printf("token '%c'\n", this->input_buf[0]);
+                consume_token(this, single_token);
+                add_char(this, c);
+            }
+
             // if (is_operator(this->input_buf[this->input_buf_size-1])) {
             //     printf("operator %c\n", this->input_buf[this->input_buf_size-1]);
                 
             // }
             printf("token '%c'\n", this->input_buf[this->input_buf_size-1]);
-            add_token(this, get_single_char_token_type(this->input_buf[0]));
+            consume_token(this, get_single_char_token_type(this->input_buf[0]));
 
             // consume 1 char, and add the next
             // consume_chars(this, 1);
-            c = fgetc(this->fp);
+            clear_buffer(this);
             add_char(this, c);
         } else if (is_delimiter(this->input_buf[this->input_buf_size-1]) && this->input_buf_size != 0/* EOF || (this->input_buf_size == 0)*/) {
             // substr = input_buf
 
             if (is_keyword(this->input_buf, this->input_buf_size)) {                
                 printf("keyword '%s'\n", this->input_buf);
-                add_token(this, Keyword);
+                consume_token(this, Keyword);
             } else if (is_int(this->input_buf, this->input_buf_size)) {
                 printf("number '%s'\n", this->input_buf);
-                add_token(this, Number);
+                consume_token(this, Number);
             } else if (is_identifier(this->input_buf, this->input_buf_size)) {
                 printf("identifier '%s'\n", this->input_buf);
-                add_token(this, Identifier);
+                consume_token(this, Identifier);
             } else {
                 printf("Unknown token: '%s'\n", this->input_buf);
             }
 
-            clear_buffer(this);
-
-            c = fgetc(this->fp);
+            // clear_buffer(this);
             add_char(this, c);
         }
     }
+
+    printf("Finished preprocessing with remaining '%s'\n", this->input_buf);
 }
